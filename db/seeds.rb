61bb322abf429d6e26a306b4ed53d35014d41339
TOYOTA momoko <toyopet-momoko@hotmail.co.jp>
2018-11-07 16:19:07 +0900

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Panda Express',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  "chinese"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category: "french"
+  },
+  {
+    name:         'Sukiya',
+    address:      'Meguro',
+    category: "japanese"
+  },
+  {
+    name:         'Coco Ichiban',
+    address:      'Asakusa',
+    category: "belgian"
+  },
+  {
+    name:         'Good Food',
+    address:      'Hiroshima',
+    category: "japanese"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
